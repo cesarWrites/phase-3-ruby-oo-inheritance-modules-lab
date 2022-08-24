4f3df01fc873574c0aa32949f845580294184b29
Sarah Mukuti <cesarwrites24@gmail.com>
2022-08-24 22:26:15 +0300

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -1,6 +1,15 @@
 require 'pry'
+require_relative './concerns/findable'
+require_relative './concerns/paramable'
+require_relative './concerns/memorable'
 
 class Song
+
+  include Memorable::InstanceMethods
+  extend Memorable::ClassMethods
+  include Paramable::InstanceMethods
+  extend Findable::ClassMethods
+
   attr_accessor :name
   attr_reader :artist
 
